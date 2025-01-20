bece44213e9f5dc97029dc4d1559140a18069edb
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-20 10:52:15 +0000

    Remove IDE specific files

--- api/models/film.py
+++ api/models/film.py
@@ -1,6 +1,7 @@
 from api.models import db
 from api.models import actor
 
+
 # A model of our actor table
 class Film(db.Model):
     film_id = db.Column(db.Integer, primary_key=True)
@@ -9,4 +10,4 @@ class Film(db.Model):
     release_year = db.Column(db.Integer, nullable=False)
     length = db.Column(db.Integer, nullable=False)
 
-    actors = db.relationship('Actor', secondary='film_actor',back_populates='films')
+    actors = db.relationship('Actor', secondary='film_actor', back_populates='films')
